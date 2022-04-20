e7137d12e22d0074641c4acac93aae73ad9a98d2
LoveLagergren <love.lagergren@elev.ga.ntig.se>
2022-04-20 09:57:14 +0200

    gg

---
+++ model/model.rb
@@ -0,0 +1,23 @@
+module Model
+    def find_articles(params)
+    end
+
+    def create_article(params)
+    end
+
+    def update_article(article_id, params)
+    end
+
+    def delete_article(article_id, params)
+    end
+
+    def get_article(params)
+    end
+
+    def login(params)
+    end
+
+    def register_user(params)
+    end
+    
+end
